85bda6d9e4c91b314c98ea95d14c8d51a0a310e4
keisha oglesby <keishaoglesby70@gmail.com>
2025-09-12 18:18:48 -0500

    v1.0.0

---
+++ functions/func.go
@@ -0,0 +1,26 @@
+package functions
+
+import (
+	"math"
+)
+
+type Vector2 struct {
+	x float64
+	y float64
+}
+type Dimens2 struct {
+	w float64
+	h float64
+}
+
+func mouseRot(mX float64, mY float64, ancX float64, ancY float64) float64 {
+	var mouse_pos Vector2 = Vector2{mX, mY}
+	var dirOfMouse float64 = float64(math.Atan2(float64(mouse_pos.y-ancY), float64(mouse_pos.x-ancX)) + math.Pi/4)
+	return dirOfMouse
+}
+func isoVec(x float64, y float64, dir float64, xSqu float64, ySqu float64) Vector2 {
+	return Vector2{
+		(x*xSqu*math.Cos(dir*math.Pi/180) - y*xSqu*math.Sin(dir*math.Pi/180)) + 400,
+		(x*ySqu*math.Sin(dir*math.Pi/180) + y*ySqu*math.Cos(dir*math.Pi/180)) + 300,
+	}
+}
---
+++ functions/go.mod
@@ -0,0 +1,3 @@
+module github.com/Kalyle-68/Go_projects
+
+go 1.24.4
