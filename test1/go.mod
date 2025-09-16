5c9fc3ed92e2f1c3d069a1fe350d5027b6661f16
keisha oglesby <keishaoglesby70@gmail.com>
2025-09-16 07:22:44 -0500

    v1.0.0

--- test1/go.mod
+++ test1/go.mod
@@ -1,3 +1,5 @@
 module github.com/Kalyle-68/Golang
 
 go 1.24.4
+
+require github.com/Kalyle-68/Golang/extras v0.0.0-20250913000335-9f979ab81674 // indirect
--- test1/main.go
+++ test1/main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"fmt"
-	"github.com/Kalyle-68/Golang/function/latest"
+	"github.com/Kalyle-68/Golang/extras"
 )
 
 func main() {
-	var vector functions.Vector2 = functions.Vector2{8, 6}
+	var vector extras.main.Vector2 = extras.main.Vector2{8, 6}
 	println(fmt.Sprint(vector.x) + ", " + fmt.Sprint(vector.y))
 }
